3170ac8383b7eab42525698d50c75437d2616bb7
ArgenTemiraliev <temiraliev0304@gmail.com>
2025-02-27 02:41:46 +0600

    header main

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -30,8 +30,8 @@ function Header() {
           <ul className="flex flex-col space-y-4 text-center">
             <li><a href="#main" className="hover:text-white transition">Home</a></li>
             <li><a href="#about" className="hover:text-white transition">About</a></li>
-            <li><a href="#contact" className="hover:text-white transition">Projects</a></li>
-            <li><a href="#project" className="hover:text-white transition">Contact</a></li>
+            <li><a href="#project" className="hover:text-white transition">Projects</a></li>
+            <li><a href="#contact" className="hover:text-white transition">Contact</a></li>
           </ul>
         </div>
       </div>
